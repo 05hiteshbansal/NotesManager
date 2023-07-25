cac7ebaccd5fa62d4243b20e981323ccdfdc6cb7
hitesh bansal <h.hiteshbansal@gmail.com>
2023-07-25 21:42:40 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Auth  from './components/Auth'
 import {Route,Routes} from "react-router-dom";
 //import Home from './components/Home';
-//import AboutUs from './components/AboutUs';
+import AboutUs from './components/AboutUs';
 import Notes from './components/Notes';
-//port Protected from './components/protected';
+import Protected from './components/Protected';
 function App() {
 
   return (
@@ -13,9 +13,9 @@ function App() {
     {/* <Route path='/' element={<Home/>}/> */}
     {/* <Route path='/login' element={<Auth/>}/> */}
     <Route path='/' element={<Auth/>}/>
-    <Route path='/notes' element={<Protected Component={Notes}/>}/>
-    {/* <Route className="container" path='/notes' element={<Notes/>}/> */}
-    {/* <Route path='/aboutUs' element={<AboutU0s/>}/> */}
+    {/* <Route  className="container" path='/notes' element={<Protected Component={Notes}/>}/> */}
+    <Route className="container" path='/notes' element={<Notes/>}/>
+    <Route path='/aboutUs' element={<AboutUs/>}/>
 
     </Routes>
 
